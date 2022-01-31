0f2e539c825cfd77f588817c9a2f8ea4f7b89130
Dmitry Abretsov <dabretsov@stepintegrator.ru>
2022-01-31 16:07:35 +0300

    ДОбавил классы

---
+++ src/main/java/Praktikum/com/Account.java
@@ -0,0 +1,23 @@
+package Praktikum.com;
+
+public class Account {
+
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        /*
+             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
+             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
+         */
+    }
+
+}
+
+
+
+
+
